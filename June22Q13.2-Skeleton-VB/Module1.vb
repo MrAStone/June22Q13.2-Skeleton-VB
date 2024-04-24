fbcc202ec57af4d3caa6e538f4116b3dd0118923
Al Stone <astone@heckgrammar.co.uk>
2024-04-24 12:08:35 +0100

    Add project files.

---
+++ June22Q13.2-Skeleton-VB/Module1.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+    ' June 2022 Q13.2 GCSE Code Problem
+    Sub Main()
+        Dim rnd As New Random
+        Dim cards = genHand(rnd)
+        Dim gameWon As Boolean = False
+
+
+    End Sub
+
+    '' This function creates the hand for you.  Don't edit this
+    Function genHand(rnd As Random) As Integer()
+        Dim handOfCards(99) As Integer
+        Dim deck(499) As Integer
+        Dim n As Integer = 1
+        For i = 0 To 499
+            deck(i) = n
+            n += 1
+            If n > 250 Then
+                n = 1
+            End If
+        Next
+        For i = 0 To 99
+            Dim num As Integer = rnd.Next(0, 500)
+            While deck(num) = -1
+                num = rnd.Next(0, 500)
+            End While
+            handOfCards(i) = deck(num)
+        Next
+        Array.Sort(handOfCards)
+        Return handOfCards
+    End Function
+End Module
---
+++ June22Q13.2-Skeleton-VB/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("June22Q13.2-Skeleton-VB")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HGS-SCCM")>
+<Assembly: AssemblyProduct("June22Q13.2-Skeleton-VB")>
+<Assembly: AssemblyCopyright("Copyright © HGS-SCCM 2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("af24911b-9b53-4883-b2c0-d0a161e62574")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
